20dee8194c5f20b1b0eaca1ad49e121c01e33d0b
KYUNGHEE YANG <alicesister1@gmail.com>
2021-03-05 00:11:39 +0900

    Django Tutorial Part 2: Creating a skeleton website.

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -1,7 +1,7 @@
-"""djangoProject URL Configuration
+"""locallibrary URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.locallibrary.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog', permanent=True)),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
