fee83328d969de99265931657ff07ae64279a44d
KYUNGHEE YANG <alicesister1@gmail.com>
2021-03-06 23:31:21 +0900

    Django Tutorial Part 4: Django admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,1 +1,47 @@
 # Register your models here.
+from django.contrib import admin
+
+from catalog.models import Book, Author, Genre, BookInstance, Language
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # author는 다대일 관계이기 때문에 __str__()값에 의해 표현됨
+    # genre는 다대다 관계이기 때문에 DB 접근비용 때문에 display_genre 함수를 대신 호출함
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'imprint', 'due_back', 'status', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        })
+    )
+
+
+admin.site.register(Genre)
+admin.site.register(Language)
--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,6 @@
 import uuid
 
 from django.db import models
-# Create your models here.
 from django.urls import reverse
 
 
@@ -34,7 +33,7 @@ class Book(models.Model):
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13,
-                            help_text='13 Character <a href=https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     # 책은 여러 개의 장르를 갖고, 장르도 여러개의 책을 가질 수 있으므로 다대다
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
@@ -44,6 +43,11 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 
 class BookInstance(models.Model):
